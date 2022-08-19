05ce1be54b19e0a410e556d5f839a000e88ea0e2
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-19 18:53:15 -0400

    Main & Print implementaions/explanation completed

--- src/Print.java
+++ src/Print.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Print {
+
+// the print method is for output
+public class Print {
+    public static void main (String [] args) {
+        // println is a method that output values or print text in java
+        // it will add a new line for each method
+        System.out.println("Hello world");
+
+        // the print method is similar to println() except that it does not insert a new line at the end of the output
+        System.out.print("Hello World");
+
+    }
 }
