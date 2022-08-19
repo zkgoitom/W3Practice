5db731d44e43c79c7845e3f5b08851323e010f0f
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-19 17:28:00 -0400

    Main. the entrypoint for every java program

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// the main method is the entry point of the class. every must contain the main() method
+public class Main {
+    public static void main (String [] args) {
+        System.out.println("Hello World");
+
+    }
+}
