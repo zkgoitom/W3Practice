6ffc995f20e98344b74a37ef6e14874df20a8d02
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-20 00:08:25 -0400

    for any files that were not added (missed) during previous staged commits

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Variables {
+// variables are temporary place holders for values
+// in Java, there are different types of variables. the type of the variable must be declared before the variable name
+// types include String, int, float, char, boolean
+public class Variables {
+
 }
