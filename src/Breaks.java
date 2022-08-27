05b181a64dcd4b683a27bf078d7bc89245cb158f
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-27 19:55:07 -0400

    practicing using the continue keyword with for/while loops in ContinueStatements.java

--- src/Breaks.java
+++ src/Breaks.java
@@ -2,20 +2,22 @@
 public class Breaks {
     public static void main (String [] args) {
         for (int i = 0; i < 10; i++) {
-            if (i == 4) {
+            if (i == 4) {   // if statement is nested in for loop (in the for loop block)
                 break;
             }
-            System.out.println(i);
+            System.out.println(i);  // statement is part of for loop block not if block
+                                    // even though the if statement is in the for loop (with the Sout statement), once the break keyword is executed looping ends
+                                    // and the Sout statement with the value of i  (which is a part of the for loop block) stops running
         }
 
         System.out.println("\n");
 
         int j = 0;
         while (j < 10) {
-            System.out.println(j);
+            System.out.println(j); // Sout statement lives in the while loop in the first line. it stops printing when break is triggered and looping ends
             j ++ ;
             if (j == 4) {
-                break;
+                break; // if statement is nested in the while loop block. when condition of if statement is met, the break keyword inside if block is executed and looping ends
             }
         }
     }
--- src/ContinueStatements.java
+++ src/ContinueStatements.java
@@ -1,2 +1,26 @@
+// continue statements break one iteration (of the loop) , if a specified condition occurs and continues with the next iteration of the loop
+// what's meant by, "breaks one iteration ..."
+// seems like continue statement make no difference. what am I missing? what's the point of using it in a loop like this?
 public class ContinueStatements {
+    public static void main (String [] args) {
+        for (int i = 0; i < 10; i ++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println('\n');
+
+        int j = 0;
+        while (j < 10) { // if, while loop statement is true execute the if statement nested in while block and as long as if statement does not end loop execute the Sout stmnt in while loop block after the  if statement then execute increment value variable. And continue looping until condition tests false
+            if (j == 4) { // if, if statement is true, increment j by 1 and then continue
+                j ++ ;      // increments in if block
+                continue;   // continue in if block
+            }
+            System.out.println(j); // Sout statement lives inside the while block, on the line after the if block
+            j ++;  // an increments value variable lives inside the while block
+
+        }
+    }
 }
