d01f30eaf5e1df1da8dab1ce7ffbab1e5cfd907a
zkgoitom <62409873+zkgoitom@users.noreply.github.com>
2022-08-27 18:51:33 -0400

    practicing looping through elements in an array with for each loops in ForEachLoops.java

---
+++ src/ForEachLoops.java
@@ -0,0 +1,9 @@
+// for each loops also known as enhanced for loops are used exclusively to loop through elements in an array
+public class ForEachLoops {
+    public static void main (String [] args) {
+        String [] players = {"Lebron", "Jordan", "Kobe", "Kareem"};
+        for (String i : players) {
+            System.out.println(i); // loops through each element
+        }
+    }
+}
